0a984fdc4f71fd0e717988a143d9b90a1c9cbf39
sandipmalii <21121a0567@vidyanikethan.asia>
2024-03-18 17:55:39 +0530

    multer & cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,26 @@
+
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+   //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname) //we can change from here also
+      
+    }
+  })
+  
+
+  
+ export const upload = multer(
+    { 
+    storage,
+}
+    )
+
+
+
+
