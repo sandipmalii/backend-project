8424969ee439e3939f014fe768a57723d53d33db
sandipmalii <21121a0567@vidyanikethan.asia>
2024-03-18 10:29:14 +0530

    connection succesfully db

--- src/index.js
+++ src/index.js
@@ -18,7 +18,15 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() =>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log('Server is running at port: ${process.env.PORT }');
+    })
+})
+.catch((err) =>{
+    console.log("MONGODB CONNECTION FAILED !!!!: ", err);
+   // process.exit(1);
+})
 
 
 
