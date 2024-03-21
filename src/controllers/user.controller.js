84c890f8e10a9ae8b3394651e68fbb517d6452ea
sandipmalii <21121a0567@vidyanikethan.asia>
2024-03-21 23:52:35 +0530

    part 1 done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,8 +27,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // Retrieve paths of avatar and cover image from the request files
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+    
+    
     // Check if avatar path exists
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required"); // Throw an error if avatar path doesn't exist
