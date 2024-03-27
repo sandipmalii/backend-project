91b4cda4fdadb3fc28cb13c60349975b94976245
sandipmalii <21121a0567@vidyanikethan.asia>
2024-03-27 16:08:13 +0530

    added route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,11 +22,22 @@
 
 // Importing necessary modules and functions
 import express from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser,
+   registerUser, 
+   refreshAccessToken, 
+   changeCurrentPassword, 
+   getCurrentUser, 
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage, 
+   getUserChannelProfile, 
+   getWatchHistory 
+  } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 // Assuming verifyJWT middleware is defined in a separate file, import it
 import { verifyJWT } from "../middlewares/auth.middleware.js"; // Adjust the path as per your actual file structure
+import { get } from "http";
 
 // Creating an instance of the Express Router
 const { Router } = express;
@@ -55,6 +66,14 @@ router.route("/login").post(loginUser);
 // Secured route for user logout
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); //patch for one
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); // for parames
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 
 // Exporting the router to be used by the application
 export default router
